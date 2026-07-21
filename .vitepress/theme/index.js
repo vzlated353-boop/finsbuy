ad120e1dba0014a989be3820df98b1ad87df77d9
vzlated353-boop <your@email.com>
2026-07-21 15:37:57 +0800

    feat: add article navigation module with 7 links to all pages

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,16 +1,19 @@
 import DefaultTheme from 'vitepress/theme'
-import { h, Fragment } from 'vue'
+import { h } from 'vue'
 import './custom.css'
 import GlobalNavbar from './components/GlobalNavbar.vue'
 import SiteFooter from './components/SiteFooter.vue'
+import ArticleNavigation from './components/ArticleNavigation.vue'
 
 export default {
   ...DefaultTheme,
   Layout() {
-    return h(Fragment, [
-      h(GlobalNavbar),
-      h(DefaultTheme.Layout),
-      h(SiteFooter)
-    ])
+    return h(DefaultTheme.Layout, null, {
+      'doc-after': () => h(ArticleNavigation),
+      'layout-bottom': () => h(SiteFooter)
+    })
+  },
+  enhanceApp({ app }) {
+    app.component('GlobalNavbar', GlobalNavbar)
   }
 }
